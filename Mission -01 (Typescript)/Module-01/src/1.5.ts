770e9dcf5262e2e7b8628ae7f422553874c6de24
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-05 19:34:31 +0600

    object type

--- Mission -01 (Typescript)/Module-01/src/1.5.ts
+++ Mission -01 (Typescript)/Module-01/src/1.5.ts
@@ -6,8 +6,15 @@ let user: {
     name: string;
     age: number;
     ismarried?: boolean; // optiona type(we can skip this but the rest of the properties are mandatory we can't skip them)
+    readonly email: string;
 } = {
     company: 'Google Inc',
     name: 'user1',
     age: 30,
+    email: 'user@google.com'
 }
+
+user.name = 'user2'; // we can re write this property value
+user.age = 32; // we can re write this property value
+// user.email = 'user2@gmail.com'; // we can't re write this property value as it is readonly
+
