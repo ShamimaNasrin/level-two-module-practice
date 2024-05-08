912059be7b5fb92307fc61f4a4cc159a77637293
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-08 18:02:05 +0600

    promise --fetch data and use type to define the data type

--- Mission -01 (Typescript)/Module-02/src/2.8.ts
+++ Mission -01 (Typescript)/Module-02/src/2.8.ts
@@ -27,11 +27,18 @@
 
      //Example: 02
 
-     const getTodo = async () => {
+     type Todo = {
+        userId: number, 
+        id: number, 
+        title: string, 
+        completed: boolean
+     }
+
+     const getTodo = async (): Promise<Todo> => {
        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
        const data = await response.json();
        console.log(data);
-
+       return data;
      };
 
      getTodo();
