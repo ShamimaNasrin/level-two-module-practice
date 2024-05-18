a1dcd9ffa6bf63d8db265b7b889b82b3f151b5fc
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-18 14:27:17 +0600

    module-6 practice task 02

---
+++ Mission -02(Advanced MongoDB)/Module-06/Practice queries/practice-1.js
@@ -0,0 +1,21 @@
+// ------------------------------------------------ Module 6-1: $Match , $Project Aggregation Stage --------------------------------------
+
+
+
+
+// -------------------------------- Module 6-2: $AddFields , $Out , $Merge Aggregation Stage ----------------------------
+// queries:
+
+
+// ------------------------------------------Module 6-3: $Group , $Sum , $Push Aggregation Stage ---------------------------------------
+// queries:
+
+
+// -----------------------------------------Module 6-4: Explore More About $Group & $Project ---------------------------------------------
+// queries:
+
+
+
+
+//-----------------------------------------------Module 6-5: Explore $Group With $Unwind Aggregation Stage -------------------------------
+// queries:
---
+++ Mission -02(Advanced MongoDB)/Module-06/Practice queries/practice-2.js
@@ -0,0 +1,21 @@
+// ------------------------------------------------ Module 6-6: $Bucket, $Sort, And $Limit Aggregation Stage -------------------------
+
+
+
+
+// -------------------------------- Module 6-7: $Facet, Multiple Pipeline Aggregation Stage ----------------------------------
+// queries:
+
+
+// ------------------------------------------Module 6-8: $Lookup Stage, Embedding Vs Referencing ---------------------------------------
+// queries:
+
+
+// -----------------------------------------Module 6-9: What Is Indexing, COLLSCAN Vs IXSCAN ---------------------------------------------
+// queries:
+
+
+
+
+//-----------------------------------------------Module 6-10: Explore Compound Index And Text Index ----------------------------------
+// queries:
