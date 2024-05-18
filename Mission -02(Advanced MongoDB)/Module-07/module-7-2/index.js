612d331cc41c767693f68f7cc355e705a8067fbb
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-19 00:35:29 +0600

    built in  module example

--- Mission -02(Advanced MongoDB)/Module-07/module-7-2/index.js
+++ Mission -02(Advanced MongoDB)/Module-07/module-7-2/index.js
@@ -4,3 +4,11 @@ const { a: a2, add: add2 } = require('./local-2')
 
 console.log(add2(3, 4, 6))
 
+
+// built-in-modules
+const path = require("path")
+
+console.log(path.dirname("D:/Web/P-Hero Next level/module-practice/Mission -02(Advanced MongoDB)/Module-07/module-7-2/index.js"));
+console.log(path.parse("D:/Web/P-Hero Next level/module-practice/Mission -02(Advanced MongoDB)/Module-07/module-7-2/index.js")); // prottekta jinis parse kore dibe
+console.log(path.join("D:/Web/P-Hero Next level/module-practice/Mission -02(Advanced MongoDB)/Module-07/module-7-2/", "local-1.js"));
+
