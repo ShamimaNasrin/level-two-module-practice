0844fac77070a6185986919210ba451b6ffa3f8e
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-19 21:46:52 +0600

    module 7-5 stream and buffer

---
+++ Mission -02(Advanced MongoDB)/Module-07/module-7-5/stream-buffer.js
@@ -0,0 +1,48 @@
+
+// Module : 7-5
+// ---------------------------------------------- Basic structure of a server -------------------------------------
+const http = require('http');
+
+//creating a server using raw node.js
+const server1 = http.createServer()
+
+// listener
+server1.on('request', (req, res) => {
+    console.log(req);
+    res.end("hello world!");
+})
+
+server1.listen(5000, () => {
+    console.log(`server is listening on port 5000`);
+})
+
+
+// ------------------------------------------------------- Streaming example -------------------------------------------------------------
+const http = require('http');
+const fs = require('fs')
+
+//creating a server using raw node.js
+const server = http.createServer()
+
+// listener
+server.on('request', (req, res) => {
+    if (req.url === '/read-file' && req.method === 'GET');
+
+    // streaming file reading
+    const readableStream = fs.createReadStream(process.cwd() + '/texts/read.txt')
+
+    // we have to listen (.on) the stream that comes like chunk by chunk  and write the response
+    readableStream.on('data', (buffer) => {
+        res.write(buffer)
+    })
+
+    // listen the process to end and when it end, end the response
+    readableStream.on('end', () => {
+        res.end('The streaming is over !')
+    })
+
+})
+
+server.listen(5000, () => {
+    console.log(`server is listening on port 5000`);
+})
