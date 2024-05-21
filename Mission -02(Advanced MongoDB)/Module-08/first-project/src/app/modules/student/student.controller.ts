0ca19adea894f8297d6805948d8e9cc536f54128
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-21 13:41:28 +0600

    m-8-9 connect router with app.ts

--- Mission -02(Advanced MongoDB)/Module-08/first-project/src/app/modules/student/student.controller.ts
+++ Mission -02(Advanced MongoDB)/Module-08/first-project/src/app/modules/student/student.controller.ts
@@ -2,17 +2,20 @@ import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
 
 const createStudent = async (req: Request, res: Response) => {
-  const student = req.body;
+  try {
+    const { student: studentData } = req.body; // using name alias for student
+    // will call service function to send this data
+    const result = await StudentServices.createStudentIntoDB(studentData);
 
-  // will call service function to send this data
-  const result = await StudentServices.createStudentIntoDB(student);
-
-  // send response
-  res.status(200).json({
-    success: true,
-    message: 'Student created successfully',
-    data: result,
-  });
+    // send response
+    res.status(200).json({
+      success: true,
+      message: 'Student created successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const StudentControllers = {
