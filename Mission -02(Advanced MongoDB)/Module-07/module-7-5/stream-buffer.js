920b13df52953e7dd9f576e0ae979ff475897ded
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-26 23:40:47 +0600

    erd and requirement analysis

--- Mission -02(Advanced MongoDB)/Module-07/module-7-5/stream-buffer.js
+++ Mission -02(Advanced MongoDB)/Module-07/module-7-5/stream-buffer.js
@@ -46,3 +46,32 @@ server.on('request', (req, res) => {
 server.listen(5000, () => {
     console.log(`server is listening on port 5000`);
 })
+
+
+// module : 7-6
+
+// --------------------------------------------------- add status code and error handling ------------------------------------------------
+server.on('request', (req, res) => {
+    if (req.url === '/read-file' && req.method === 'GET');
+
+    // streaming file reading
+    const readableStream = fs.createReadStream(process.cwd() + '/texts/readbhul.txt')
+
+    readableStream.on('data', (buffer) => {
+        res.statusCode = 200;
+        res.write(buffer)
+    })
+
+    readableStream.on('end', () => {
+        res.statusCode = 200;
+        res.end('The streaming is over !')
+    })
+
+    readableStream.on('error', (error) => {
+        console.log(error);
+        res.statusCode = 500;
+        res.end('Something went wrong!')
+    })
+
+
+})
