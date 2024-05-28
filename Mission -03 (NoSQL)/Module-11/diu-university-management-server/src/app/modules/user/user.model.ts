0234de72cc7234d4a4988c6d87f935d53beb1669
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-29 00:22:58 +0600

    module-11-7 user interface, model, validation

---
+++ Mission -03 (NoSQL)/Module-11/diu-university-management-server/src/app/modules/user/user.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['student', 'faculty', 'admin'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true, // for createdAt and updatedAt properties(mongoose will create these with the timestamp itself)
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
