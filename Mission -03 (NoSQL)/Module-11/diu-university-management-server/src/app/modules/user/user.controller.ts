099cf19bf4603bd4f80c037edcce8a87040e1f7f
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-29 11:06:03 +0600

    module 11-9 create user and add user id and _id to the student data

---
+++ Mission -03 (NoSQL)/Module-11/diu-university-management-server/src/app/modules/user/user.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { UserServices } from './user.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { password, student: studentData } = req.body;
+
+    // data validation with zod
+    // const zodParsedData = studentValidationSchema.parse(studentData);
+
+    const result = await UserServices.createStudentIntoDB(
+      password,
+      studentData,
+    );
+
+    // send response
+    res.status(200).json({
+      success: true,
+      message: 'Student created successfully',
+      data: result,
+    });
+  } catch (err: any) {
+    // console.log(err);
+    res.status(500).json({
+      success: false,
+      message: err.message || 'something went wrong',
+      error: err,
+    });
+  }
+};
+
+export const UserControllers = {
+  createStudent,
+};
