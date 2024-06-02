ecf545d1a1ed985288bb0864c0acabce1d1aa904
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-06-02 13:31:33 +0600

    m-12-5 craete model for academic semester

---
+++ Mission -03 (NoSQL)/Module-12/diu-university-management-server/src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,66 @@
+import { Schema, model } from 'mongoose';
+import {
+  TAcademicSemester,
+  TAcademicSemesterCode,
+  TAcademicSemesterName,
+  TMonths,
+} from './academicSemester.interface';
+
+const Months: TMonths[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const AcademicSemesterName: TAcademicSemesterName[] = [
+  'Autumn',
+  'Summar',
+  'Fall',
+];
+const AcademicSemesterCode: TAcademicSemesterCode[] = ['01', '02', '03'];
+
+const acdemicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterName,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterCode,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicSemesterModel = model<TAcademicSemester>(
+  'AcademicSemester',
+  acdemicSemesterSchema,
+);
