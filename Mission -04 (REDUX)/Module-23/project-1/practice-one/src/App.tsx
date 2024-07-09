53f029fa228da8de86c7d0a352b4ff3cb181af93
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-07-09 22:57:41 +0600

    module-23-3 use object as state

--- Mission -04 (REDUX)/Module-23/project-1/practice-one/src/App.tsx
+++ Mission -04 (REDUX)/Module-23/project-1/practice-one/src/App.tsx
@@ -1,9 +1,12 @@
+import UserWithUseState from "./components/UserWithUseState";
+
 function App() {
   return (
     <>
       <div>
         {" "}
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <UserWithUseState></UserWithUseState>
       </div>
     </>
   );
