bf954d3bb433ea9c6e7343843623b31aaa317a6a
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-07-12 00:22:34 +0600

    m-23-8 handle todo form

--- Mission -04 (REDUX)/Module-23/practice-one/src/components/ToDoForm.tsx
+++ Mission -04 (REDUX)/Module-23/practice-one/src/components/ToDoForm.tsx
@@ -1,12 +1,40 @@
-import React, { useContext } from "react";
+import React, { FormEvent, useContext, useState } from "react";
 import { TodoContext } from "../context/TodoProvider";
 
 const ToDoForm = () => {
-  const { todoTilte } = useContext(TodoContext);
-  console.log("todoTilte:", todoTilte);
+  const { state, dispatch } = useContext(TodoContext);
+  const [task, setTask] = useState("");
+
+  console.log("state:", state);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random().toString(36).substring(2, 7),
+      title: task,
+      isComplete: false,
+    };
+
+    dispatch({ type: "addTodo", payload: todo });
+  };
+
   return (
     <div>
-      <h1>form component..!!</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Todo">Task</label>
+        <input
+          onBlur={(e) => setTask(e.target.value)}
+          className="border border-purple-300 m-10"
+          type="text"
+          name="todo"
+          id="todo"
+        />
+
+        <button className="border py-1 px-2 bg-purple-300" type="submit">
+          submit
+        </button>
+      </form>
     </div>
   );
 };
