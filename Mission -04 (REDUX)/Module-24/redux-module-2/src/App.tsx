64abcf14b90b77b0d5d325218ce91df0fa0aa41e
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-07-14 19:52:34 +0600

    m-24-4 initialize redux store

--- Mission -04 (REDUX)/Module-24/redux-module-2/src/App.tsx
+++ Mission -04 (REDUX)/Module-24/redux-module-2/src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
+          <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">
+            Increment
+          </button>
+          <h1 className="text-3xl mx-10">0</h1>
+          <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white">
+            Decrement
+          </button>
+        </div>
+      </div>
     </>
   );
 }
