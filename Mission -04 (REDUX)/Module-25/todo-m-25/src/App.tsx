50e0da7c6e63769112001ce2cd793f6f43a5be90
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-07-31 21:23:26 +0600

    m-25-3 make basic todo card and container

--- Mission -04 (REDUX)/Module-25/todo-m-25/src/App.tsx
+++ Mission -04 (REDUX)/Module-25/todo-m-25/src/App.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button";
+import Todo from "./pages/Todo";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
+      <Todo></Todo>
     </>
   );
 }
---
+++ Mission -04 (REDUX)/Module-25/todo-m-25/src/components/todo/TodoCard.tsx
@@ -0,0 +1,16 @@
+const TodoCard = () => {
+  return (
+    <div className="bg-white p-3 flex justify-between items-center rounded-md ">
+      <input type="checkbox" name="" id="" />
+      <p className="font-semibold">Todo Title</p>
+      <p>Time</p>
+      <p>Description</p>
+      <div className="space-x-5">
+        <button>del</button>
+        <button>edit</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoCard;
