088f54f0e1e71ef5fa239e77c34d1a82ce081abf
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-08-01 21:16:56 +0600

    m-25-4 add modal and dropdown from shadcn

--- Mission -04 (REDUX)/Module-25/todo-m-25/src/components/todo/TodoContainer.tsx
+++ Mission -04 (REDUX)/Module-25/todo-m-25/src/components/todo/TodoContainer.tsx
@@ -1,20 +1,24 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal />
+        <Button>Filter</Button>
       </div>
-      <div className="bg-slate-600 w-full h-full rounded-xl p-5 space-y-3">
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px] ">
         {/* <div className="bg-white p-5 flex justify-center items-center rounded-md text-2xl font-bold">
           {" "}
           <p>There is no pending task</p>{" "}
         </div> */}
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
       </div>
     </div>
   );
--- Mission -04 (REDUX)/Module-25/todo-m-25/src/components/ui/Container.tsx
+++ Mission -04 (REDUX)/Module-25/todo-m-25/src/components/ui/Container.tsx
@@ -5,9 +5,7 @@ type TContainerProps = {
 };
 
 const Container = ({ children }: TContainerProps) => {
-  return (
-    <div className="h-screen w-full max-w-7xl mx-auto border">{children}</div>
-  );
+  return <div className="h-screen w-full max-w-7xl mx-auto ">{children}</div>;
 };
 
 export default Container;
