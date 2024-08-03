fc47056fb9629c13868a8b547dc1fa5ffda35a09
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-08-03 22:10:19 +0600

    m-26-1 todo card refactor

---
+++ Mission -04 (REDUX)/Module-26/todo-m-26/src/components/todo/AddTodoModal.tsx
@@ -0,0 +1,84 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { FormEvent, useState } from "react";
+import { useAppDispatch } from "@/redux/hook";
+import { addTodo } from "@/redux/features/todoSlice";
+
+const AddTodoModal = () => {
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // console.log(task);
+    // console.log(description);
+
+    const randomStr = Math.random().toString(36).substring(2, 9);
+    const taskDetails = {
+      id: randomStr,
+      title: task,
+      description,
+    };
+    // console.log(taskDetails);
+
+    dispatch(addTodo(taskDetails));
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="bg-primary-gradient text-xl font-semibold">
+          Add todo
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add task</DialogTitle>
+          <DialogDescription>Add tasks you want to finish.</DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="task" className="text-right">
+                Task
+              </Label>
+              <Input
+                onBlur={(e) => setTask(e.target.value)}
+                id="task"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="Description" className="text-right">
+                Description
+              </Label>
+              <Input
+                onBlur={(e) => setDescription(e.target.value)}
+                id="Description"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-end">
+            <DialogClose asChild>
+              <Button type="submit">Save changes</Button>
+            </DialogClose>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTodoModal;
---
+++ Mission -04 (REDUX)/Module-26/todo-m-26/src/components/todo/TodoContainer.tsx
@@ -0,0 +1,38 @@
+// import { useAppSelector } from "@/redux/hook";
+import AddTodoModal from "./AddTodoModal";
+import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
+
+const TodoContainer = () => {
+  // from local state
+  // const { todos } = useAppSelector((state) => state.todoList);
+  // from local server
+  const { data: todos, isLoading, isError } = useGetTodosQuery(undefined);
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+  return (
+    <div>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal />
+        {/* <Button>Filter</Button> */}
+        <TodoFilter />
+      </div>
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px] ">
+        {/* <div className="bg-white p-5 flex justify-center items-center rounded-md text-2xl font-bold">
+          {" "}
+          <p>There is no pending task</p>{" "}
+        </div> */}
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+          {todos?.data?.map((item, i) => (
+            <TodoCard key={i} {...item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoContainer;
