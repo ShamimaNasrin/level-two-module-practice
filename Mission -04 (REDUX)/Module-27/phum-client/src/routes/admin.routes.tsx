8786fe8692d24033eaaee4a22155de7f08d702bf
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-09-06 00:02:40 +0600

    m-27-8 resolve ts error

--- Mission -04 (REDUX)/Module-27/phum-client/src/routes/admin.routes.tsx
+++ Mission -04 (REDUX)/Module-27/phum-client/src/routes/admin.routes.tsx
@@ -1,7 +1,13 @@
+import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+
+type TRoute = {
+  path: string;
+  element: ReactNode;
+};
 
 //combine array for route and side menu items
 const adminPaths = [
@@ -33,7 +39,7 @@ const adminPaths = [
 ];
 
 //* Programatical way
-export const adminRoutes = adminPaths.reduce((acc, item) => {
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
   if (item.path && item.element) {
     acc.push({
       path: item.path,
