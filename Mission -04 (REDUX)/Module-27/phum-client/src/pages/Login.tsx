10d0a7e69a344e7db05cbb6678c565e19b52d646
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-09-10 19:37:12 +0600

    m-28-5 login form

--- Mission -04 (REDUX)/Module-27/phum-client/src/pages/Login.tsx
+++ Mission -04 (REDUX)/Module-27/phum-client/src/pages/Login.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div>login</div>;
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0002",
+      password: "admin123",
+    },
+  });
+
+  const [login, { error }] = useLoginMutation();
+
+  const onSubmit = async (data) => {
+    console.log("data:", data);
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
--- Mission -04 (REDUX)/Module-27/phum-client/src/redux/api/baseApi.ts
+++ Mission -04 (REDUX)/Module-27/phum-client/src/redux/api/baseApi.ts
@@ -1,8 +1,10 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/api/v1",
+  }),
 
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
   endpoints: () => ({}),
 });
